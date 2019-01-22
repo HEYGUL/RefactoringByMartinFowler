758d4d83b2ad2147841284df9b7ae77eef9743d8
Guillaume Lagorce <glagorce@octo.com>
2019-01-22 23:04:33 +0100

    Enrich `performances` with `aPerformance`

--- statement.js
+++ statement.js
@@ -3,8 +3,13 @@ module.exports = statement;
 function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
+  statementData.performances = invoice.performances.map(enrichPerformance);
   return renderPlainText(statementData, plays);
+}
+
+function enrichPerformance(aPerformance) {
+  const result = Object.assign({}, aPerformance);
+  return result;
 }
 
 function renderPlainText(data, plays) {
