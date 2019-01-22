dfcf379d9941053fa9ef8285e15616b272fe0941
Guillaume Lagorce <glagorce@octo.com>
2019-01-22 18:20:42 +0100

    `Change function declaration` - rename `format` to `usd` and factorize the division by 100

--- statement.js
+++ statement.js
@@ -35,12 +35,12 @@ function statement(invoice, plays) {
     return result;
   }
 
-  function format(aNumber) {
+  function usd(aNumber) {
     return new Intl.NumberFormat("en-US",
       {
         style: "currency", currency: "USD",
         minimumFractionDigits: 2
-      }).format(aNumber);
+      }).format(aNumber / 100);
   }
 
   let totalAmount = 0;
@@ -50,10 +50,10 @@ function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
-    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
