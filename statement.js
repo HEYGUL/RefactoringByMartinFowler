51c40cdc165e8527e94ba675e8587841b3a2e0ef
Guillaume Lagorce <glagorce@octo.com>
2019-01-22 22:36:04 +0100

    `Extract Function` - extract `totalAmount` in dedicated function

--- statement.js
+++ statement.js
@@ -6,13 +6,19 @@ function statement(invoice, plays) {
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   }
-  let totalAmount = 0;
-  for (let perf of invoice.performances) {
-    totalAmount += amountFor(perf);
-  }
+  let totalAmount = appleSauce();
+
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
+
+  function appleSauce() {
+    let totalAmount = 0;
+    for (let perf of invoice.performances) {
+      totalAmount += amountFor(perf);
+    }
+    return totalAmount;
+  }
 
   function amountFor(aPerformance) {
     let result = 0;
