cb577d0a0ed7067de6ee2f93b06ae1824556168b
Guillaume Lagorce <glagorce@octo.com>
2019-01-22 22:52:08 +0100

    Create intermediate data structure between computation and rendering

--- statement.js
+++ statement.js
@@ -1,10 +1,11 @@
 module.exports = statement;
 
 function statement(invoice, plays) {
-  return renderPlainText(invoice, plays);
+  const statementData = {};
+  return renderPlainText(statementData, invoice, plays);
 }
 
-function renderPlainText(invoice, plays) {
+function renderPlainText(data, invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
     // print line for this order
