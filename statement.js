6d3c922fbe3b2bc43e5741ce300ee887fbc34bc6
Guillaume Lagorce <glagorce@octo.com>
2019-01-22 18:28:30 +0100

    `Slide Statements` - Move `volumeCredits` variable declaration next to the loop where it is used

--- statement.js
+++ statement.js
@@ -44,13 +44,13 @@ function statement(invoice, plays) {
   }
 
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
+  let volumeCredits = 0;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
   }
