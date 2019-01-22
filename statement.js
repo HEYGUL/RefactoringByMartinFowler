778a5cd9b113fc1095510717caea4c3028c53c92
Guillaume Lagorce <glagorce@octo.com>
2019-01-22 23:01:00 +0100

    `Delete Parameter` - delete useless `invoice` parameter of `statement` function

--- statement.js
+++ statement.js
@@ -4,10 +4,10 @@ function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances;
-  return renderPlainText(statementData, invoice, plays);
+  return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(data, invoice, plays) {
+function renderPlainText(data, plays) {
   let result = `Statement for ${data.customer}\n`;
   for (let perf of data.performances) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
