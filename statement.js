4baef92a5074fcd79c821c0557339066afedfe5b
Guillaume Lagorce <glagorce@octo.com>
2019-01-22 22:45:32 +0100

    `Extract Function` - extract rendering in dedicated `renderPlainText` function

--- statement.js
+++ statement.js
@@ -1,6 +1,10 @@
 module.exports = statement;
 
 function statement(invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
     // print line for this order
