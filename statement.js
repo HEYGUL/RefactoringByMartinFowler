9ecf17ccfabb0e533c7f94f42daf39017916472f
Guillaume Lagorce <glagorce@octo.com>
2019-01-22 18:14:01 +0100

    `Rename variable` - rename `perf` to `aPerformance` in newly created function `volumeCreditsFor`

--- statement.js
+++ statement.js
@@ -27,11 +27,11 @@ function statement(invoice, plays) {
     return plays[aPerformance.playID];
   }
 
-  function volumeCreditsFor(perf) {
+  function volumeCreditsFor(aPerformance) {
     let volumeCredits = 0;
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    volumeCredits += Math.max(aPerformance.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(aPerformance).type) volumeCredits += Math.floor(aPerformance.audience / 5);
     return volumeCredits;
   }
 
