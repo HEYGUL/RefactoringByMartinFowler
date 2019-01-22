5047bac239b0e149a0fb30e92f462d2652a7b5e8
Guillaume Lagorce <glagorce@octo.com>
2019-01-22 17:46:27 +0100

    `Inline Variable` - replace `play` variable by `playFor` function inside `amountFor` function

--- statement.js
+++ statement.js
@@ -3,7 +3,7 @@ module.exports = statement;
 function statement(invoice, plays) {
   function amountFor(aPerformance, play) {
     let result = 0;
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -18,7 +18,7 @@ function statement(invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`unknown type: ${play.type}`);
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
     return result;
   }
