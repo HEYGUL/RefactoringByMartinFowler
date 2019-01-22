fdfbb10b14ee42285ab93acc54deaecacdae0792
Guillaume Lagorce <glagorce@octo.com>
2019-01-22 17:47:23 +0100

    `Change Function Declaration` - delete `play` parameter of `amountFor` function

--- statement.js
+++ statement.js
@@ -1,7 +1,7 @@
 module.exports = statement;
 
 function statement(invoice, plays) {
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
     switch (playFor(aPerformance).type) {
       case "tragedy":
@@ -36,7 +36,7 @@ function statement(invoice, plays) {
       minimumFractionDigits: 2
     }).format;
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
