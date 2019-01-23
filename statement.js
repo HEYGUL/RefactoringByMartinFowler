751be13e17baa3866358892ac07220f0ca2036b9
Guillaume Lagorce <glagorce@octo.com>
2019-01-23 22:50:56 +0100

    Use `performance` from `data`structure

--- statement.js
+++ statement.js
@@ -19,7 +19,7 @@ function statement(invoice, plays) {
   function renderPlainText(data, plays) {
   let result = `Statement for ${data.customer}\n`;
   for (let perf of data.performances) {
-    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+    result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   }
   result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
@@ -35,7 +35,7 @@ function statement(invoice, plays) {
 
   function amountFor(aPerformance) {
     let result = 0;
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -50,7 +50,7 @@ function statement(invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+        throw new Error(`unknown type: ${aPerformance.play.type}`);
     }
     return result;
   }
@@ -58,8 +58,7 @@ function statement(invoice, plays) {
     function volumeCreditsFor(aPerformance) {
       let result = 0;
       result += Math.max(aPerformance.audience - 30, 0);
-      // add extra credit for every ten comedy attendees
-    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+      if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
       return result;
     }
 
