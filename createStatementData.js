1440c030bc316558a64f826e6cf8d2b079b50092
Guillaume Lagorce <glagorce@octo.com>
2019-01-23 23:45:08 +0100

    Rename returned variable to `result`

--- createStatementData.js
+++ createStatementData.js
@@ -1,12 +1,12 @@
 module.exports = createStatementData;
 
 function createStatementData(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return statementData;
+  const result = {};
+  result.customer = invoice.customer;
+  result.performances = invoice.performances.map(enrichPerformance);
+  result.totalAmount = totalAmount(result);
+  result.totalVolumeCredits = totalVolumeCredits(result);
+  return result;
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
