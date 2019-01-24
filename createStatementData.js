9400dddffa6e74289d449dc73e0604d227f18ae6
Guillaume Lagorce <glagorce@octo.com>
2019-01-24 17:04:19 +0100

    Add `get amount` function to `PerformanceCalculator` class

--- createStatementData.js
+++ createStatementData.js
@@ -5,6 +5,27 @@ class PerformanceCalculator {
     this.performance = aPerformance;
     this.play = aPlay;
   }
+  get amount() {
+    let result = 0;
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${this.play.type}`);
+    }
+    return result;
+  }
 }
 
 function createStatementData(invoice, plays) {
