d6126598bb6adaa2f055de228a4d0ca4e83208c3
Guillaume Lagorce <glagorce@octo.com>
2019-01-24 17:13:26 +0100

    Inline Function - inline `volumeCredits` usage

--- createStatementData.js
+++ createStatementData.js
@@ -47,7 +47,7 @@ function createStatementData(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
     result.amount = calculator.amount;
-    result.volumeCredits = volumeCreditsFor(result);
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
   function playFor(aPerformance) {
@@ -58,8 +58,5 @@ function createStatementData(invoice, plays) {
   }
   function totalAmount(data) {
     return data.performances.reduce((total, p) => total + p.amount, 0);
-  }
-  function volumeCreditsFor(aPerformance) {
-    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
   }
 }
