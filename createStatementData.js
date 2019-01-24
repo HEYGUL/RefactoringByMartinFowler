22b5a59d71ff48a49e6d4d6f8d80d3424ce57fc6
Guillaume Lagorce <glagorce@octo.com>
2019-01-24 22:50:52 +0100

    Create subclass for comedy and tragedy performance calculator

--- createStatementData.js
+++ createStatementData.js
@@ -34,8 +34,19 @@ class PerformanceCalculator {
   }
 }
 
+class TragedyCalculator extends PerformanceCalculator {
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+}
+
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
+  switch (aPlay.type) {
+    case 'tragedy': return new TragedyCalculator(aPerformance, aPlay);
+    case 'comedy': return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`unknown type: ${aPlay.type}`)
+  }
 }
 
 function createStatementData(invoice, plays) {
