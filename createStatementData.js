2b072daaa18383bbb0c8af0de38f6ce187cba35d
Guillaume Lagorce <glagorce@octo.com>
2019-01-24 22:59:36 +0100

    `Replace Conditional with Polymorphism` - calculate amount for comedies

--- createStatementData.js
+++ createStatementData.js
@@ -11,12 +11,7 @@ class PerformanceCalculator {
       case "tragedy":
         throw 'bad thing';
       case "comedy":
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
+        throw 'bad thing';
       default:
         throw new Error(`unknown type: ${this.play.type}`);
     }
@@ -41,6 +36,14 @@ class TragedyCalculator extends PerformanceCalculator {
 }
 
 class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
 }
 
 function createPerformanceCalculator(aPerformance, aPlay) {
