5ae418f81cdd3a6a11ff6a43bbd380db553870f8
Guillaume Lagorce <glagorce@octo.com>
2019-01-24 23:05:42 +0100

    Move volumeCredits specific case to `ComedyCalculator` class

--- createStatementData.js
+++ createStatementData.js
@@ -9,10 +9,7 @@ class PerformanceCalculator {
     throw new Error('subclass responsability');
   }
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
-    if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
-    return result;
+    return Math.max(this.performance.audience - 30, 0);
   }
 }
 
@@ -34,6 +31,9 @@ class ComedyCalculator extends PerformanceCalculator {
     }
     result += 300 * this.performance.audience;
     return result;
+  }
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
 
