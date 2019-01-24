261799b0555c7b17c64a74521f6672062866ad91
Guillaume Lagorce <glagorce@octo.com>
2019-01-24 22:56:07 +0100

    `Replace Conditional with Polymorphism` - calculate amount for tragedies

--- createStatementData.js
+++ createStatementData.js
@@ -9,11 +9,7 @@ class PerformanceCalculator {
     let result = 0;
     switch (this.play.type) {
       case "tragedy":
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
+        throw 'bad thing';
       case "comedy":
         result = 30000;
         if (this.performance.audience > 20) {
@@ -35,6 +31,13 @@ class PerformanceCalculator {
 }
 
 class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
 }
 
 class ComedyCalculator extends PerformanceCalculator {
